36911509119dba8332a8cee3bdbdf6b5e6982968
Codingfolders <sundays2477@gmail.com>
2026-05-03 17:57:07 +0900

    v3.0.0 update

--- README.md
+++ README.md
@@ -1,33 +1,100 @@
 # OhoConsole
 > Simple, colorful, and clean console logger for Node.js
 
-A lightweight and simple console logging library
+가볍고 간단한 콘솔 로깅 라이브러리
 
-[한국어](README_ko.md) | [English](README.md)
+[⌈github repository⌋](https://github.com/Codingfolders/oho-console/) | [⌈npm library ¦ 3.0.0⌋](https://www.npmjs.com/package/oho-console) | [⌈한국어⌋](#한국어) | [⌈English⌋](#english)
+
+## 한국어
+
+Node.js 22버전 이상까지 지원합니다!
+
+### 주요 기능
+- **자동 타임스탬프: `[ 15:30:00 INFO ]` 형식으로 출력 시간 자동 표시**
+- **로그 레벨 식별: INFO, WARN, ERROR 등으로 레벨 출력**
+- **색상 지원: 빨간색, 노란색, 파란색, 초록색, 흰색 색상 적용으로 가독성 향상**
+- **중복 메시지 방지: 이전과 동일한 메시지 출력 생략으로 가독성 향상**
+
+### 사용 예시
+#### 예시 1
+- 코드
+``` js
+const { msg, ohoSettings } = require('oho-console');
+
+ohoSettings.TextColor('yellow'); // 기본 설정: white
+msg.info('노란색 문자 메시지입니다!'); // 기본 색상으로 출력
+msg.info('빨간색 문자 메시지입니다!', 'red'); // 빨간색으로 출력
+```
+- 출력
+```
+[ 15:30:00 INFO ] 노란색 문자 메시지입니다!
+[ 15:30:00 INFO ] 빨간색 문자 메시지입니다!
+```
+
+#### 예시 2
+- 코드
+``` js
+const { msg, ohoSettings } = require('oho-console');
+
+msg.info('이 메시지는 중복되었습니다!'); // 기본 색상으로 출력
+msg.info('이 메시지는 중복되었습니다!'); // 출력 건너뜀
+
+ohoSettings.returnDuplicateMessageEnabled(false); // 기본 설정: true
+
+msg.info('이 메시지는 중복되었습니다!'); // 기본 색상으로 출력
+msg.info('이 메시지는 중복되었습니다!');
+```
+- 출력
+```
+[ 15:30:00 INFO ] 이 메시지는 중복되었습니다!
+[ 15:30:00 INFO ] 이 메시지는 중복되었습니다!
+[ 15:30:00 INFO ] 이 메시지는 중복되었습니다!
+```
+
+-----
 
 ## English
-✨ Current Version: 2.0.2
+
+Supports Node.js version 22 or higher!
 
 ### Key Features 
-- **Automatic Timestamp: Displays the time in `[ 15:30 INFO ]` format**
+- **Automatic Timestamp: Displays the time in `[ 15:30:00 INFO ]` format**
 - **Log Level Identification: Clearly distinguishes log levels such as INFO, WARN, and ERROR**
 - **Color Support: Enhances readability with Red, Yellow, Blue, Green, and White**
 - **Duplicate Message Prevention: Keep your console clean by skipping identical consecutive messages**
 
 ### Usage Example
-#### Code
+#### Example 1
+- Code
 ``` js
-const { msg, setColor } = require('oho-console');
+const { msg, ohoSettings } = require('oho-console');
 
-setColor('yellow');
-msg('info', 'This is a yellow text message!'); // Outputs in the default color
-msg('info', 'This is a red text message!', 'red'); // Outputs in red
-msg('log', 'This message is a duplicate!');
-msg('log', 'This message is a duplicate!'); // Outputs skipped
+ohoSettings.TextColor('yellow'); // default Settings: white
+msg.info('This is a yellow text message!'); // Outputs in the default color
+msg.info('This is a red text message!', 'red'); // Outputs in red
 ```
-#### Output
+- Output
 ```
-[ 15:30 INFO ] This is a yellow text message!
-[ 15:30 INFO ] This is a red text message!
-[ 15:30 LOG ] This message is a duplicate!
+[ 15:30:00 INFO ] This is a yellow text message!
+[ 15:30:00 INFO ] This is a red text message!
+```
+
+#### Example 2
+- Code
+``` js
+const { msg, ohoSettings } = require('oho-console');
+
+msg.info('This message is a duplicate!'); // Outputs in the default color
+msg.info('This message is a duplicate!'); // Outputs skipped
+
+ohoSettings.returnDuplicateMessageEnabled(false); // default Settings: true
+
+msg.info('This message is a duplicate!'); // Outputs in the default color
+msg.info('This message is a duplicate!');
+```
+- Output
+```
+[ 15:30:00 INFO ] This message is a duplicate!
+[ 15:30:00 INFO ] This message is a duplicate!
+[ 15:30:00 INFO ] This message is a duplicate!
 ```
--- function/lib.js
+++ function/lib.js
@@ -1,66 +1,119 @@
-const defaultTextColor = '\x1b[0m';
-let TextColor = '\x1b[0m';
-
+// Environment Variables
 let lastMessage = null;
 
-// =====
-function getTime () {
+let settings = require('#settings.json');
+
+// sub function
+function MessageFormatComponent (level, message, color) {
+    // get OutputTime
     const CurrentTime = new Date();
     const OutputTime = {
         "hours": CurrentTime.getHours(),
         "minutes": CurrentTime.getMinutes(),
         "seconds": CurrentTime.getSeconds()
     }
-    return OutputTime;
+    
+    // Initial Variable
+    const InitialColorCode = '\x1b[0m';
+    
+    // Format config
+    const DisplayTime = `${String(OutputTime.hours).padStart(2, '0')}:${String(OutputTime.minutes).padStart(2, '0')}:${String(OutputTime.seconds).padStart(2, '0')}`;
+    
+    const finalColor = color || settings?.TextColor || '\x1b[37m' || 'white';
+    const ColorCode = getColorCode(finalColor);
+
+    const FormatComponent = `[${DisplayTime} ${level}] ${message}`
+    const finalFormatComponent = `${ColorCode}${FormatComponent}${InitialColorCode}`
+    
+    return finalFormatComponent;
 }
 
-function getColor (color) {
-    const validColor = [
-        'red',
-        'yellow',
-        'blue',
-        'green',
-        'white'
-    ]
-    const Colors = {
-        'red': '\x1b[31m',
-        'yellow': '\x1b[33m',
-        'blue': '\x1b[34m',
-        'green': '\x1b[32m',
-        'white': '\x1b[0m'
+function getColorCode (color = 'white') {
+    const colorsCode = {
+        "red": "\x1b[31m",
+        "yellow": "\x1b[33m",
+        "blue": "\x1b[34m",
+        "green": "\x1b[32m",
+        "white": "\x1b[37m"
     }
 
-    const isValid = validColor.includes(color);
-    if (!color) return;
-    if (!isValid) return;
-
-    return Colors[color];
+    if (!colorsCode[color]) return colorsCode['white'];
+    return colorsCode[color];
 }
 
-// =====
-
-function msg (level, message, color) {
-    // Get Time & 
-    const Time = getTime();
-
-    if (lastMessage && lastMessage === message) return;
-    lastMessage = message;
+// main function
+const ohoSettings = {
+    TextColor: function (color = 'white') {
+        const colorsName =  [
+            "red",
+            "yellow",
+            "blue",
+            "green",
+            "white"
+        ]
+        if (!colorsName.includes(color)) return;
     
-    //
-    const DisplayTime = `${String(Time.hours).padStart(2, '0')}:${String(Time.minutes).padStart(2, '0')}:${String(Time.seconds).padStart(2, '0')}`;
-    const DisplayLevel = level.toUpperCase();
-
-    if (color) console.log(getColor(color) + `[ ${DisplayTime} ${DisplayLevel} ] ${message}` + defaultTextColor);
-    else console.log(TextColor + `[ ${DisplayTime} ${DisplayLevel} ] ${message}` + defaultTextColor);
-
-    if (!lastMessage || lastMessage !== message) lastMessage = message;
+        settings.TextColor = color;
+    },
+    returnDuplicateMessageEnabled: function (isEnabled = true) {
+        if (typeof isEnabled !== 'boolean') return;
+        settings.returnDuplicateMessageEnabled = isEnabled;
+    }
 }
 
-function setColor (color) {
-    TextColor = getColor(color);
+const msg = {
+    log: function (message = '', color = settings.TextColor, isImportant = false) {
+        // If the last message and the current message match (ignore duplicate when not isImportant)
+        if (!isImportant && settings.returnDuplicateMessageEnabled && message === lastMessage) return;
+
+        const level = 'LOG';
+        const Format = MessageFormatComponent(level, message, color, isImportant);
+
+        console.log(Format);
+
+        // last message update
+        lastMessage = message;
+    },
+    info: function (message = '', color = settings.TextColor, isImportant = false) {
+        // If the last message and the current message match (ignore duplicate when not isImportant)
+        if (!isImportant && settings.returnDuplicateMessageEnabled && message === lastMessage) return;
+
+        const level = 'INFO';
+        const Format = MessageFormatComponent(level, message, color, isImportant);
+
+        console.log(Format);
+
+        // last message update
+        lastMessage = message;
+    },
+    warn: function (message = '', color = settings.TextColor, isImportant = false) {
+        // If the last message and the current message match (ignore duplicate when not isImportant)
+        if (!isImportant && settings.returnDuplicateMessageEnabled && message === lastMessage) return;
+
+        const level = 'WARN';
+        const Format = MessageFormatComponent(level, message, color, isImportant);
+
+        console.log(Format);
+
+        // last message update
+        lastMessage = message;
+    },
+    error: function (message = '', color = settings.TextColor, isImportant = false) {
+        // If the last message and the current message match (ignore duplicate when not isImportant)
+        if (!isImportant && settings.returnDuplicateMessageEnabled && message === lastMessage) return;
+
+        const level = 'ERROR';
+        const Format = MessageFormatComponent(level, message, color, isImportant);
+
+        console.log(Format);
+
+        // last message update
+        lastMessage = message;
+1    }
 }
 
+// module exports
 module.exports = {
-    msg,
-    setColor
+    ohoSettings,
+    msg
 }
